a3ee903538c41c1e40d120980d51ce4ad5457115
Larissa-Santos16 <larisan970@gmail.com>
2024-10-10 17:22:10 -0300

    Implementa formulário de cadastro com validação de campos e integração ViaCep

--- src/store/store.js
+++ src/store/store.js
@@ -1,34 +1,12 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import createPersistedState from "vuex-persistedstate";
-import axios from 'axios'; // Certifique-se de importar o Axios
-
-Vue.use(Vuex);
+import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import auth from "@/store/modules/auth";
+import viaCep from "@/store/modules/viaCep";
 
 export default new Vuex.Store({
-  state: {
-    user: null, // Você pode armazenar informações do usuário aqui
-    error: null, // Para armazenar mensagens de erro
-  },
-  mutations: {
-    SET_USER(state, user) {
-      state.user = user;
-    },
-    SET_ERROR(state, error) {
-      state.error = error;
-    },
-  },
-  actions: {
-    async Register({ commit }, user) {
-      try {
-        const response = await axios.post('http://localhost:8000/api/user/create', user); 
-        commit('SET_USER', response.data); 
-      } catch (error) {
-        commit('SET_ERROR', error.response?.data?.message || "Erro ao registrar");
-          throw error; 
-          
-      }
-    },
+  modules: {
+    auth,
+    viaCep,
   },
   plugins: [createPersistedState()],
 });
