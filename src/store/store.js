8c7554858504a2b0c11d1680753d4a00332c87b4
Larissa-Santos16 <larisan970@gmail.com>
2024-10-09 21:15:13 -0300

    Configuracao do Store para armazenar o User

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,34 @@
+import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
-// import gender from "@/store/modules/gender";
+import axios from 'axios'; // Certifique-se de importar o Axios
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {
-        // gender,
-
+  state: {
+    user: null, // Você pode armazenar informações do usuário aqui
+    error: null, // Para armazenar mensagens de erro
+  },
+  mutations: {
+    SET_USER(state, user) {
+      state.user = user;
     },
-    plugins: [createPersistedState()],
+    SET_ERROR(state, error) {
+      state.error = error;
+    },
+  },
+  actions: {
+    async Register({ commit }, user) {
+      try {
+        const response = await axios.post('http://localhost:8000/api/user/create', user); 
+        commit('SET_USER', response.data); 
+      } catch (error) {
+        commit('SET_ERROR', error.response?.data?.message || "Erro ao registrar");
+          throw error; 
+          
+      }
+    },
+  },
+  plugins: [createPersistedState()],
 });
